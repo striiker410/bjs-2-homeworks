b4eceb05968a79034cff7917996b0d1b2569dcfa
Леонид Мостовой <108294042+striiker410@users.noreply.github.com>
2023-04-19 16:34:53 +0300

    Первое и второе задание готовы

---
+++ task.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+function solveEquation(a, b, c) {
+  let arr = [];
+  let total = (b ** 2) - 4 * a * c;
+  
+  if (total < 0) {
+    arr = [];
+  } else if (total === 0) {
+    arr = [-b / ( 2 * a )];
+  } else {
+    arr = [(-b + Math.sqrt(total) ) / (2*a), (-b - Math.sqrt(total) ) / (2*a)];
+  }
+
+  return arr;
+}
+
+
+
+
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {  
+  
+  percent = percent/100/12;
+  let creditBody = amount - contribution;
+  let monthPay = creditBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+  let totalPay = monthPay * countMonths;
+  
+  totalPay = Number(totalPay.toFixed(2));
+
+  return totalPay;
+}
+
+
+
+
