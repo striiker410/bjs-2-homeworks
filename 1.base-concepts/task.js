682bf3ced6ed246c20a2320e03157ff63002f806
Леонид Мостовой <108294042+striiker410@users.noreply.github.com>
2023-04-20 18:09:15 +0300

    Первая задача исправлена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
+
+//Задача №1
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let total = (b ** 2) - 4 * a * c;
+
+  if (total === 0) {
+    arr = [-b / (2 * a)];
+  } else if (total > 0) {
+    arr = [(-b + Math.sqrt(total)) / (2 * a), (-b - Math.sqrt(total)) / (2 * a)]
+    ;
+  }
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+solveEquation(1,2,4);
+
+//Задача №2
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {  
   
+  percent = percent/100/12;
+  let creditBody = amount - contribution;
+  let monthPay = creditBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+  let totalPay = monthPay * countMonths;
+  
+  totalPay = Number(totalPay.toFixed(2));
+
+  return totalPay;
 }
