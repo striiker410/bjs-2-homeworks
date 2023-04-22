ec430204c531541b7338d3ee058060326bef99f0
Леонид Мостовой <108294042+striiker410@users.noreply.github.com>
2023-04-22 13:10:12 +0300

    Первое и второе задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
+
+
+//Задача №1
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
+
+
+//Задача №2
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter( user => user.gender === gender).reduce((acc, current, index, array) => acc + current.age / array.length, 0);
+    return result;
 }
